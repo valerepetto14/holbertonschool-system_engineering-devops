076c09d834fb8b5e9d8e5b2163cfa9eb29ebea16
valentin repetto <valentinrepetto12345@gmail.com>
2022-08-14 14:46:33 +0000

    add 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# automatizar la taks 2
+file_line { 'Identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
+file_line { 'disable password login':
+    path    => '/etc/ssh/ssh_config',
+    line    => '    PasswordAuthentication no',
+}
