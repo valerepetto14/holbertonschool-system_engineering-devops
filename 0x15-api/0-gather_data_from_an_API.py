50fe550cc944d5c5389197b46895c712fffd34ba
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-06 22:21:52 +0000

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,24 +6,22 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    try:
-        id = sys.argv[1]
-        ul = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
-        ul1 = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
-        data = requests.get(ul)
-        todos = requests.get(ul1)
+    id = sys.argv[1]
+    ul = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    ul1 = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
+    data = requests.get(ul)
+    todos = requests.get(ul1)
 
-        name = data.json().['name']
-        titles = []
-        done = 0
-        all = 0
-        for i in todos.json():
-            all += 1
-            if i.get('completed') is True:
-                done += 1
-                title.append(i['title'])
-        print('Employee {} is done with tasks({}/{}):'.format(name, done, all))
-        for tarea in titles:
-            print("\t {}".format(tarea))
-    except Exception:
-        print(Exception)
+    name = data.json().['name']
+    titles = []
+    done = 0
+    all = 0
+    for i in todos.json():
+        all += 1
+        if i.get('completed') is True:
+            done += 1
+            title.append(i['title'])
+    print('Employee {} is done with tasks({}/{}):'.format(name, done, all))
+    for tarea in titles:
+        print("\t {}".format(tarea))
+
