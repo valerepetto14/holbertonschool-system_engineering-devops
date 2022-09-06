47bacc37c7388cc8827cb906ae62967e7d38bb55
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-06 22:54:01 +0000

    add 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,14 +11,19 @@ if __name__ == '__main__':
         name_file = '{}.json'.format(id)
         ul = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
         ul1 = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
-        data = requests.get(ul)
+        user = requests.get(ul)
         tareas = requests.get(ul1)
+        name = user.json()['name']
         lista_task = []
-        dic = {}
+        dic_final = {}
         with open(name_file, 'w', encoding='utf-8') as f:
             for tarea in tareas.json():
-                lista_task.append(tarea)
-            dic[id] = lista_task
-            f.write(json.dumps(dic))
+                dic_tareas = {}
+                dic_tareas['task'] = tarea['title']
+                dic_tareas['completed'] = tarea['completed']
+                dic_tareas['username'] = name
+                lista_task.append(dic_tareas)
+            dic_final[id] = lista_task
+            f.write(json.dumps(dic_final))
     except Exception as e:
         print(e)
