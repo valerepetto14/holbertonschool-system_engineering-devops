4fb5ce60f83f249a35eb305bc3c6ee681323bc08
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-06 22:34:49 +0000

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,25 +6,21 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    try:
-        id = sys.argv[1]
-        ul = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
-        ul1 = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
-        data = requests.get(ul)
-        todos = requests.get(ul1)
+    id = sys.argv[1]
+    ul = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    ul1 = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
+    data = requests.get(ul)
+    todos = requests.get(ul1)
 
-        EMPLOYEE_NAME = data.json()['name']
-        TOTAL_NUMBER_OF_TASKS = 0
-        NUMBER_OF_DONE_TASKS = 0
-        TASK_TITLE = []
-        for i in todos.json():
-            TOTAL_NUMBER_OF_TASKS += 1
-            if i.get('completed') is True:
-                NUMBER_OF_DONE_TASKS += 1
-                TASK_TITLE.append(i['title'])
-        print('Employee {} is done with tasks({}/{}):'.format(EMPLOYEE_NAME,
-              NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-        for tarea in TASK_TITLE:
-            print("\t {}".format(tarea))
-    except Exception:
-        print(Exception)
+    name = data.json()['name']
+    titles = []
+    done_tasks = 0
+    all_tasks = 0
+    for i in todos.json():
+        all_tasks += 1
+        if i.get('completed') is True:
+            done_tasks += 1
+            titles.append(i['title'])
+    print("Employee {} is done with tasks({}/{}):".format(name, done_tasks, all_tasks))
+    for task in titles:
+        print("\t {}".format(task))
