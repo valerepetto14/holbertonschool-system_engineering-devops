37feff34df8290a1b37b3ce2c695d81a6e817c5f
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-06 23:00:18 +0000

    add comments 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,19 +11,28 @@ if __name__ == '__main__':
         name_file = '{}.json'.format(id)
         ul = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
         ul1 = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
+        """hago las request"""
         user = requests.get(ul)
         tareas = requests.get(ul1)
+
+        """defino variables a usar"""
         username = user.json()['username']
         lista_task = []
         dic_final = {}
+    
         with open(name_file, 'w', encoding='utf-8') as f:
             for tarea in tareas.json():
+                """reseteo el dic para volver a llenarlo"""
                 dic_tareas = {}
+                """lleno el dic con sus valores"""
                 dic_tareas['task'] = tarea['title']
                 dic_tareas['completed'] = tarea['completed']
                 dic_tareas['username'] = username
+                """agrego el dic a la lista final"""
                 lista_task.append(dic_tareas)
+            """agrego la lista al value de la key id"""
             dic_final[id] = lista_task
+            """serealizo el diccionario y lo escribo en el documento .json"""
             f.write(json.dumps(dic_final))
     except Exception as e:
         print(e)
